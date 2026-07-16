d7418af3e235dabae0056156db7a702af2f5881f
Namanhack <147086418+naman0867@users.noreply.github.com>
2026-07-17 00:43:07 +0530

    Time: 2 ms (35.69%), Space: 47.2 MB (57.88%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                Queue<Integer> queue = new LinkedList<>();
+                queue.offer(i);
+                color[i] = 0;
+                while (!queue.isEmpty()) {
+                    int u = queue.poll();
+                    for (int v : graph[u]) {
+                        if (color[v] == -1) {
+                            color[v] = color[u] ^ 1;
+                            queue.offer(v);
+                        } else if (color[v] == color[u]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
