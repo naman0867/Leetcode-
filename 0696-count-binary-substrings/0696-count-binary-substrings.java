68febbc9e0e72dfaa80aba574ed82d8938b2f7e6
Namanhack <147086418+naman0867@users.noreply.github.com>
2026-07-17 00:39:57 +0530

    Time: 11 ms (51.74%), Space: 46.2 MB (85.78%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int prev = 0, cur = 1, count = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                cur++;
+            } else {
+                count += Math.min(prev, cur);
+                prev = cur;
+                cur = 1;
+            }
+        }
+        count += Math.min(prev, cur);
+        return count;
+    }
+}
