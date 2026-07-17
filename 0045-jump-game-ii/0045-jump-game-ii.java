22fb4ba9f52c2bd0bf9bcc4c6f293bf523806e6a
Namanhack <147086418+naman0867@users.noreply.github.com>
2026-07-17 19:01:26 +0530

    Time: 1 ms (99.74%), Space: 47.6 MB (8.69%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int totalJumps = 0;
         int destination = nums.length-1;
         int coverage = 0,lastIndex = 0;
-        if(nums.length == 1d) return 0;
+        if(nums.length == 1) return 0;
         for(int i=0;i<nums.length;i++){
             coverage = Math.max(coverage,i+nums[i]);
             if(i == lastIndex){
