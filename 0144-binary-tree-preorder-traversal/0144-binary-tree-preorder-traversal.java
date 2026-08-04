a4e621db263951bc90e620551d053bd84304c064
Namanhack <147086418+naman0867@users.noreply.github.com>
2026-08-04 16:51:42 +0530

    Time: 1 ms (6.74%), Space: 43.3 MB (15.04%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            ans.add(node.val);
+            if (node.right != null) stack.push(node.right); 
+            if (node.left != null) stack.push(node.left);
+        }
+        return ans;
+    }
+}
